a989ccdb51c4fae69670ff1aee8ab15ea9558570
Jeff Crockett <jeffreyneilcrockett@gmail.com>
2018-07-31 10:26:18 -0400

    Done.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,8 @@
+require 'pry'
 def clock_angle(time)
-    # code your solution here
+    hour_minute = time.split(':').map{|x| x.to_i}
+    # binding.pry
+    solution_one = 0.5 * (60 * hour_minute[0] + hour_minute[1]) - 6 * hour_minute[1]
+    solution_two = 360 - (0.5 * (60 * hour_minute[0] + hour_minute[1]) - 6 * hour_minute[1])
+    solution_one < solution_two ? solution_one : solution_two
 end
