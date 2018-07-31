588c43294d2bb4eb5015c84215621459d99b2561
aubreeabril <aubree.abril@gmail.com>
2018-07-31 10:28:13 -0400

    solving

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,7 @@
 def clock_angle(time)
-    # code your solution here
+    h = time.split(':')[0].to_i
+    m = time.split(':')[1].to_i
+    solution1 = 0.5 * (60 * h + m) - 6 * m
+    solution2 = 360 - (0.5 * (60 * h + m) - 6 * m)
+    solution1 < solution2 ? solution1 : solution2
 end
